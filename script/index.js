3fa441b38c231608dab7e861af2dbd302530b0c0
나경 <skrud9550@naver.com>
2025-02-25 18:13:00 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,16 @@
+const mainSwiper = new Swiper(".pf_wrap", {
+    direction: "vertical",
+    slidesPerView: 1,
+    mousewheel: true,
+    speed: 700,
+});
+
+// const designSwiper = new Swiper('.web_design_wrap', {
+//     scrollbar: {
+//         el: ".swiper-scrollbar",
+//     },
+//     slidesPerView: 1,
+//     autoplay: {delay: 600,},
+//     loop: true,
+    
+// })
